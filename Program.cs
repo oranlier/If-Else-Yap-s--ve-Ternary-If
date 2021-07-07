96e2c5bd15bd6d1d03501125319a09e8b5d31bb7
burak oranlier <oranlier@hotmail.com>
2021-07-07 23:00:50 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace If_Else_Yapısı_ve_Ternary_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >=6 && time<=11)
+                Console.WriteLine("Günaydın!");
+            else if (time<=18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi Geceler!");
+
+            string sonuc = time>=6 && time<=11 ? "Günaydın" : time<=18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
